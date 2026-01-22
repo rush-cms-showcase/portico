376264bce46f1a60a0e64b95bc10bd0178327018
Rafhael Marsigli <rmarsigli@gmail.com>
2026-01-22 20:12:20 -0300

    feat: add unplugin-icons support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import Icons from 'unplugin-icons/vite'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    vite: {
+        plugins: [
+            Icons({
+                compiler: 'astro',
+            }),
+        ],
+    },
+})
