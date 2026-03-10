c27199f56bc47013a32dc3d7c288de69b6c778d1
Rafhael Marsigli <rmarsigli@gmail.com>
2026-03-10 15:10:54 -0300

    feat(seo): install and configure @astrojs/sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,18 @@ import { defineConfig } from 'astro/config'
 import Icons from 'unplugin-icons/vite'
 import tailwindcss from '@tailwindcss/vite'
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [
-            tailwindcss(),
-            Icons({
-                compiler: 'astro',
-            }),
-        ],
-    },
+  vite: {
+      plugins: [
+          tailwindcss(),
+          Icons({
+              compiler: 'astro',
+          }),
+      ],
+  },
+  site: 'https://porticoreformas.com.br',
+  integrations: [sitemap()],
 })
